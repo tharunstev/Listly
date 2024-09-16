7dd2c5d5ba357f72bb42302f6af6f0c66e4722e3
tharunstev <163376561+tharunstev@users.noreply.github.com>
2024-09-16 17:30:01 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,11 @@ import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import NewTaskDialogBox from "./NewTaskDialogBox";
 import EditTaskDialogBox from "./EditTaskDialogBox";
-import Stack from '@mui/material/Stack';
-import LinearProgress from '@mui/material/LinearProgress';
-import Snackbar from '@mui/material/Snackbar';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const WhiteButton = styled(Button)`
   color: white;
@@ -22,13 +21,13 @@ const WhiteButton = styled(Button)`
   }
 `;
 
-const Container = styled('div')`
+const Container = styled("div")`
   display: flex;
   flex-direction: column;
   padding: 20px;
 `;
 
-const ButtonsContainer = styled('div')`
+const ButtonsContainer = styled("div")`
   display: flex;
   justify-content: flex-end;
   gap: 10px;
@@ -40,7 +39,7 @@ const ButtonsContainer = styled('div')`
   }
 `;
 
-const TaskContainer = styled('div')`
+const TaskContainer = styled("div")`
   display: flex;
   flex-direction: column;
   gap: 10px;
@@ -51,23 +50,23 @@ const TaskContainer = styled('div')`
 
   & .task-item {
     display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  word-wrap: break-word;
-  padding: 10px;
-  border-radius: 5px;
-  background-color: #262A4A;
-  margin-bottom: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  width: 100%;
+    flex-direction: column;
+    align-items: flex-start;
+    word-wrap: break-word;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: #262a4a;
+    margin-bottom: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    width: 100%;
   }
 
   & .task-buttons {
     display: flex;
-  justify-content: flex-end;
-  gap: 10px;
-  margin-top: 10px;
-  width: 100%;
+    justify-content: flex-end;
+    gap: 10px;
+    margin-top: 10px;
+    width: 100%;
   }
 `;
 
@@ -75,37 +74,37 @@ const Home = ({ input }) => {
   const { tasks, loading, error } = useSelector((state) => state.task);
   const dispatch = useDispatch();
 
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState("all");
   const [openDialog, setOpenDialog] = useState(false);
   const [openEditDialog, setOpenEditDialog] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
 
-  
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   useEffect(() => {
     dispatch(getTasks());
   }, [dispatch]);
 
-  if (loading) return (
-    <Stack sx={{ width: '100%', color: 'grey.500', mt: "100px" }} spacing={4}>
-      <LinearProgress color="secondary" />
-      <h1>WELCOME TO LISTLY...</h1>
-      <LinearProgress color="secondary" />
-      <h1>STREAMLINE YOUR TASKS WITH LISTLY...</h1>
-      <LinearProgress color="success" />
-      <h1>ORGANIZE, ACHIEVE, REPEAT WITH LISTLY...</h1>
-      <LinearProgress color="inherit" />
-      <h1>LISTLY PROVIDES A WAY TO ORGANIZE YOUR DAILY TASKS...</h1>
-      <LinearProgress color="inherit" />
-    </Stack>
-  );
+  if (loading)
+    return (
+      <Stack sx={{ width: "100%", color: "grey.500", mt: "100px" }} spacing={4}>
+        <LinearProgress color="secondary" />
+        <h1>WELCOME TO LISTLY...</h1>
+        <LinearProgress color="secondary" />
+        <h1>STREAMLINE YOUR TASKS WITH LISTLY...</h1>
+        <LinearProgress color="success" />
+        <h1>ORGANIZE, ACHIEVE, REPEAT WITH LISTLY...</h1>
+        <LinearProgress color="inherit" />
+        <h1>LISTLY PROVIDES A WAY TO ORGANIZE YOUR DAILY TASKS...</h1>
+        <LinearProgress color="inherit" />
+      </Stack>
+    );
 
   if (error) return <p>Error: {error}</p>;
 
-  const filteredTasks = tasks.filter(task => {
-    const filterTask = filter === 'all' ? true : task.completed;
+  const filteredTasks = tasks.filter((task) => {
+    const filterTask = filter === "all" ? true : task.completed;
     const inputSearch = task.todo.toLowerCase().includes(input.toLowerCase());
     return filterTask && inputSearch;
   });
@@ -117,24 +116,25 @@ const Home = ({ input }) => {
   const handleAddTask = (newTask) => {
     dispatch(addTask(newTask))
       .then(() => {
-        setSnackbarMessage('Task added successfully');
+        setSnackbarMessage("Task added successfully");
         setSnackbarOpen(true);
       })
       .catch(() => {
-        setSnackbarMessage('Failed to add task');
+        setSnackbarMessage("Failed to add task");
         setSnackbarOpen(true);
       });
     setOpenDialog(false);
   };
 
   const handleDeleteTask = (taskId) => {
+    console.log("handle button is triggered");
     dispatch(deleteTask(taskId))
       .then(() => {
-        setSnackbarMessage('Task deleted successfully');
+        setSnackbarMessage("Task deleted successfully");
         setSnackbarOpen(true);
       })
       .catch(() => {
-        setSnackbarMessage('Failed to delete task');
+        setSnackbarMessage("Failed to delete task");
         setSnackbarOpen(true);
       });
   };
@@ -142,23 +142,29 @@ const Home = ({ input }) => {
   const handleMarkAsDone = (taskId) => {
     dispatch(updateTask({ taskId, completed: true }))
       .then(() => {
-        setSnackbarMessage('Well Done , You Did It ');
+        setSnackbarMessage("Well Done , You Did It ");
         setSnackbarOpen(true);
       })
       .catch(() => {
-        setSnackbarMessage('Failed to mark task as done');
+        setSnackbarMessage("Failed to mark task as done");
         setSnackbarOpen(true);
       });
   };
 
   const handleEditTask = (taskId, updatedTodo) => {
-    dispatch(updateTask({ taskId, completed: selectedTask.completed, todo: updatedTodo }))
+    dispatch(
+      updateTask({
+        taskId,
+        completed: selectedTask.completed,
+        todo: updatedTodo,
+      })
+    )
       .then(() => {
-        setSnackbarMessage('Task edited successfully');
+        setSnackbarMessage("Task edited successfully");
         setSnackbarOpen(true);
       })
       .catch(() => {
-        setSnackbarMessage('Failed to edit task');
+        setSnackbarMessage("Failed to edit task");
         setSnackbarOpen(true);
       });
     setOpenEditDialog(false);
@@ -179,7 +185,6 @@ const Home = ({ input }) => {
       aria-label="close"
       color="inherit"
       onClick={handleCloseSnackbar}
-      
     >
       <CloseIcon fontSize="small" />
     </IconButton>
@@ -188,31 +193,62 @@ const Home = ({ input }) => {
   return (
     <Container>
       <ButtonsContainer>
-        {filter !== 'all' && (
-          <WhiteButton variant="outlined" onClick={() => taskTypeHandler('all')}>Tasks</WhiteButton>
+        {filter !== "all" && (
+          <WhiteButton
+            variant="outlined"
+            onClick={() => taskTypeHandler("all")}
+          >
+            Tasks
+          </WhiteButton>
         )}
-        {filter !== 'completed' && (
-          <WhiteButton variant="outlined" onClick={() => taskTypeHandler('completed')}>Completed</WhiteButton>
+        {filter !== "completed" && (
+          <WhiteButton
+            variant="outlined"
+            onClick={() => taskTypeHandler("completed")}
+          >
+            Completed
+          </WhiteButton>
         )}
-        <WhiteButton variant="outlined" onClick={() => setOpenDialog(true)}>Add Task</WhiteButton>
+        <WhiteButton variant="outlined" onClick={() => setOpenDialog(true)}>
+          Add Task
+        </WhiteButton>
       </ButtonsContainer>
 
       <TaskContainer>
         {filteredTasks.length > 0 ? (
           filteredTasks.map((task) => (
             <div className="task-item" key={task.id}>
-              <h1 style={{ color: "white", fontWeight: "300", fontSize: "26px" }}>{task.todo}</h1>
+              <h1
+                style={{ color: "white", fontWeight: "300", fontSize: "26px" }}
+              >
+                {task.todo}
+              </h1>
               <div className="task-buttons">
                 {!task.completed && (
-                  <WhiteButton variant="outlined" onClick={() => handleMarkAsDone(task.id)}>Done</WhiteButton>
+                  <WhiteButton
+                    variant="outlined"
+                    onClick={() => handleMarkAsDone(task.id)}
+                  >
+                    Done
+                  </WhiteButton>
                 )}
-                <WhiteButton variant="outlined" onClick={() => openEditDialogHandler(task)}>Edit</WhiteButton>
-                <WhiteButton variant="outlined" onClick={() => handleDeleteTask(task.id)}>Delete</WhiteButton>
+                <WhiteButton
+                  variant="outlined"
+                  onClick={() => openEditDialogHandler(task)}
+                >
+                  Edit
+                </WhiteButton>
+                <WhiteButton
+                  variant="outlined"
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  Delete
+                </WhiteButton>
               </div>
             </div>
           ))
         ) : (
-          <p>No tasks available</p>
+          <h1>No tasks available!!...</h1>
         )}
       </TaskContainer>
 
@@ -232,19 +268,12 @@ const Home = ({ input }) => {
       )}
 
       <Snackbar
-      
         open={snackbarOpen}
-        
-        autoHideDuration={6000}
+        autoHideDuration={3000}
         onClose={handleCloseSnackbar}
         message={snackbarMessage}
         action={action}
-        
-        
-      /> 
-      
-      
-      
+      />
     </Container>
   );
 };
